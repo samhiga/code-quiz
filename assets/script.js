9f677befdd3ed8195773920b1b6d6673213b6fba
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-03 18:47:49 -0700

    added more ids and classes for different divs in HTML and began adding questions in JS

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+ var questions = [
+     {
+         question: "Commonly used data types DO NOT include:",
+         choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+         answer: "c. alerts"
+     }
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
+        answer: "d. <script>"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        answer: "c. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },]
+
+ 
