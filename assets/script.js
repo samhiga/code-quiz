f4a11ce806a8208f2570320a73d41f3818a766c4
Sam Higa <samhiga@Sams-MacBook-Pro.local>
2023-04-03 20:15:06 -0700

    began working on timer in JS, added query selectors and var to get timer working

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
- var questions = [
+var containerStartEl = document.getElementById("start-container")
+var containerStartEl = document.getElementById("question-container")
+var btnStartEl = document.querySelector ("#start");
+var timerEl = document.querySelector ("#timer");
+var timeleft;
+timerEl.innerText = 0;
+
+
+var questions = [
      {
          question: "Commonly used data types DO NOT include:",
          choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
          answer: "c. alerts"
-     }
+     },
     {
         question: "Inside which HTML element do we put the JavaScript?",
         choices: ["a. <js>", "b. <javascript>", "c. <scripting>", "d. <script>"],
@@ -18,6 +26,30 @@
         question: "Arrays in JavaScript can be used to store _____.",
         choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
         answer: "b. other arrays"
-    },]
+    },
+];
+var startPage = function () {
+    timerEl.textContent
+}
 
- 
+
+var setTime = function () {
+    timeleft = 60;
+
+var timercheck = setInterval(function() {
+    timerEl.innerText = timeleft;
+    timeleft--
+
+    if (gameover) {
+        clearInterval(timercheck)
+    }
+   
+    if (timeleft < 0) {
+        showScore()
+        timerEl.innerText = 0
+        clearInterval(timercheck)
+    }
+
+    }, 1000)
+}
+
